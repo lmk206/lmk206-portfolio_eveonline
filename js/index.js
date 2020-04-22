3b288bf2d000209cb939ab5daf445094b725d184
lmk206 <59675250+lmk206@users.noreply.github.com>
2020-04-22 13:09:44 +0900

    eveonline
    200422

--- js/index.js
+++ js/index.js
@@ -24,7 +24,6 @@ var mqP = window.matchMedia("screen and (min-width: 1025px)");
         if($(window).width() > 319 && $(window).width() < 768 ){
             $('.visual figure img').not(":first").hide();
             $('#main')[0].pause();
-            // $("#magin")[0].hide();
             mobileSlide();
         }
     }
